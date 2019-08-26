87616437cdb11a5ca997728eccc6ac9e753d0a2c
sepantos <54162073+sepantos@users.noreply.github.com>
2019-08-26 14:24:02 +0700

    ujian
    ujian pertama

---
+++ ujian2.js
@@ -0,0 +1,21 @@
+var alfabet = "a" ;
+var nama = 'andri';
+
+if((nama != "" ) && (alfabet = "a")){
+    console.log("masuk gruop pertama")
+}
+else if((nama != "") && (alfabet = "b")){
+    console.log("masuk group pertama")
+}
+else if((nama != "") && (alfabet = "c")){
+    console.log("masuk grup pertama")
+}
+else if((nama != "") && (alfabet= "d")){
+    console.log("masuk group pertama")
+}
+else if((namaa != "") && (alfabet= "e")){
+    console.log("masuk group kedua")
+}
+else{
+    console.log("tidak ada kelompok")
+}
---
+++ ujian3.js
@@ -0,0 +1,6 @@
+
+function processSentece(name1, name2, name3, name4){
+    return "Mr" + name1 +",Mr "+name2+",Mr "+name3+",Mr "+name4;
+}
+var fullSentence = processSentece("Steve Fox", "Jin Kazama", "Eddie GOrdo", "Geese Howard");
+console.log(fullSentence);
